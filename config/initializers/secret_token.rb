fa3f6e37f12590897c5bf3a2dbabb1467d3b34db
supertopher <asimplechris@gmail.com>
2013-07-17 10:21:41 -0700

    Initial postgres db commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FashionOverflow::Application.config.secret_token = '03db4e9ef3599f3010f20258985cb02727345834eb75936392ddb1c26e65852325c071f7c8b833d6c19f67a318af07f4425f8c7c4c7b1da22f33b7811055baa9'
