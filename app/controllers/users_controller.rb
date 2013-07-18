c654248514e64b8aa5d07a8fcaa01921521bc1d0
= <=>
2013-07-17 17:43:47 -0700

    Removed misnamed file in layout

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
 
   def index
-    
+
   end
 
   def create
     @user = User.create(params[:user])
     login(@user)
     redirect_to root_path
-    # redirect_to session_url action: 'new' 
     puts "Users#create"
   end
 
@@ -24,5 +23,6 @@ class UsersController < ApplicationController
   end
 
   
-  
+
+
 end
--- app/views/layouts/_navigation.rb
+++
@@ -1,5 +0,0 @@
-<div class="navigation">
-  <% if current_user %>
-    <%= link_to 'Logout', session_path(current_user), :method => :delete %>
-  <% end %>
-</div>
