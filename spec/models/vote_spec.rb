326a9196f15f90aa42f7b3e21d98e2e0d0b5e2e2
Apprentice <apprentice@devbootcamp.com>
2013-07-18 15:22:18 -0700

    edited vote spec

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,9 +1,26 @@
 require 'spec_helper'
 
 describe Vote do
+	let(:user_id)   { 1 }
+  let(:crush_id)  { 1 }
+  let(:up)        { true }
+  let(:vote)      { Vote.create(user_id: user_id, crush_id: crush_id, up: up) }
 
-  context do
-    
-  end
+	context "validations" do
+		it "should allow for :up to be true" do 
+			expect(vote).to be_valid
+		end
+
+		it "should allow for :up to be false" do 
+			expect(Vote.create(user_id: user_id, crush_id: crush_id, up: false)).to be_valid
+		end
+
+		# # this isn't throwing an error...
+		# this saves with a string....weirdd
+		# it "should not allow for :up to be a string" do 
+		# 	expect(Vote.create(user_id: user_id, crush_id: crush_id, up: "string")).to be_invalid
+		# end
+
+	end
 
 end
