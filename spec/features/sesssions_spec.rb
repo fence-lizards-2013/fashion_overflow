352ae63a107beb17e171b664751f7d88a929793f
supertopher <asimplechris@gmail.com>
2013-07-17 19:59:08 -0700

    Added tests for error handling

--- spec/features/sesssions_spec.rb
+++ spec/features/sesssions_spec.rb
@@ -16,7 +16,30 @@ describe "Sessions" do
     fill_in "username",  with: ""
     fill_in "password",  with: "honey"
     click_button "Login"
+    current_path.should eq sessions_path
     page.should have_content("An error occured, please try again.")
   end
 
+  it "displays errors on blank password" do
+    visit "/sessions/new"
+    fill_in "username",  with: "topher"
+    fill_in "password",  with: ""
+    click_button "Login"
+    current_path.should eq sessions_path
+    page.should have_content("An error occured, please try again.")
+  end
+
+  it "displays errors with blank e-mail and blank password" do
+    visit "/sessions/new"
+    click_button "Login"
+    current_path.should eq sessions_path
+    page.should have_content("An error occured, please try again.")
+  end
+
+  it "displays errors with correct username, WRONG password" do
+    this_user = User.create(username: "topher", password: "password")
+    click_button "Login"
+    current_path.should eq sessions_path
+    page.should have_content("An error occured, please try again.")
+  end
 end
