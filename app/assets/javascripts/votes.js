1bfe71d93b59fc807d0c7bc673d041bccc0f84b4
dbcapprentice <dbcapprentice@dbc.com>
2013-07-21 18:17:05 -0700

    fixed event delegation

--- app/assets/javascripts/votes.js
+++ app/assets/javascripts/votes.js
@@ -1,32 +1,38 @@
 $(document).ready(function() {
-
-  $('a#vote').on('ajax:success', function(e, response) {
-    $(this).text($(this).data('text') + ' ' + response.new_count);
-    var $outerClass = $(this).parents().eq(2);
-    var $current = $(this).parents().eq(2).data().aggregate;
-
-    if ($(this).data().text === "upvotes") { 
-      $outerClass.attr('data-aggregate', $current+1) 
-    };
-    if ($(this).data().text === "downvotes") { 
-      $outerClass.attr('data-aggregate', $current-1) 
-    };
-    
+  function animateVoteChange(){
     var $clone = $('.photo_block').children().clone();
-    var $sortedClone = $clone.clone().tsort({attr:'data-aggregate', order:'desc'});
+    var $sortedClone = $clone.tsort({attr:'data-aggregate', order:'desc'});
     var $container = $('.photo_block');
     $container.children().remove();
     $container.append($sortedClone.each(function(index,photo){
       $photo = $(photo)
       $photo.fadeIn('fast');
     }));
+  }
 
-    // debugger
-    // $('.photo_block').children().tsort({attr:'data-aggregate', order:'desc'});
+  function changeAggregate(target, response){
+    var $link = $(target);
+    $link.text($link.data('text') + ' ' + response.new_count);
+    var $wholePhoto = $link.parents().eq(2);
+    var $current = $link.parents().eq(2).data().aggregate;
+
+    if($link.data().text === "upvotes"){ 
+      $wholePhoto.attr('data-aggregate', $current+1);
+    }else if($link.data().text === "downvotes"){ 
+      $wholePhoto.attr('data-aggregate', $current-1);
+    }else {
+      alert("An error occurred");
+    }
+  }
+
+  $('.photo_block').on('ajax:success', 'a#vote',function(e, response) {
+    console.log(response);
+    changeAggregate($(this), response);
+    animateVoteChange();
   });
 
-
-  $('a#vote').on('ajax:error', function(e, xhr) {
+  $('.photo_block').on('ajax:error', 'a#vote',function(e, xhr) {
+    console.log('got here!');
     alert("You already voted, DUMBASS !");
   });
 });
